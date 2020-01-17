f42a1edf63571955e662129b372670f55e61c8d7
Rein Duque <rey.duqueii@gmail.com>
2020-01-17 11:09:35 +0000

    PO changes

--- ibas_tech1/__manifest__.py
+++ ibas_tech1/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','stock','sale','sales_team','account','web'],
+    'depends': ['base','stock','sale','sales_team','account','web','purchase', 'purchase_stock'],
 
     # always loaded
     'data': [
@@ -29,6 +29,7 @@
         'views/view_order_form.xml',
         'views/view_move_form.xml',
         'report/report_templates.xml',
+        'report/purchase_report.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
